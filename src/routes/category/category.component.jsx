dc7229113d3417c7434368c29a99a8ccd14cde17
Peter Weiser <peterweiser87@gmail.com>
2023-02-15 17:24:08 +0200

    Basic functions are finished.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -14,9 +14,12 @@ const Category = () => {
     }, [category, categoriesMap])
 
     return (
-        <div className='category-container'>
-            {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
-        </div>
+        <Fragment>
+            <h2 className='category-title' >{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
+            </div>
+        </Fragment>
     )
 }
 
