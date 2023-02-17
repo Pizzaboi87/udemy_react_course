8c33a497c74e500c7e3c3b42452399d37c117356
Peter Weiser <peterweiser87@gmail.com>
2023-02-18 00:26:28 +0200

    Created categories in redux.

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import App from './App'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
-import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context'
 import { store } from './store/store'
 
@@ -12,11 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
