b8d6ff27d2ee15f7a460b3a20d780c9c4a989fa3
Peter Weiser <peterweiser87@gmail.com>
2023-02-20 20:57:53 +0200

    Redux Thunk and Spinner added.

--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,17 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+import thunk from 'redux-thunk'
 
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean)
 
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
