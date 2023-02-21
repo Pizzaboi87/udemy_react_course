dbfa88690c9a363e95548bec844591c839c4d738
Peter Weiser <peterweiser87@gmail.com>
2023-02-21 22:24:58 +0200

    Redux Saga migration finished.

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -1,12 +1,20 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects'
 import { USER_ACTION_TYPES } from './user.types'
-import { signInSuccess, signInFailed, signUpSuccess, signUpFailed } from './user.action'
+import { 
+    signInSuccess, 
+    signInFailed, 
+    signUpSuccess, 
+    signUpFailed, 
+    signOutSuccess, 
+    signOutFailed 
+} from './user.action'
 import { 
     getCurrentUser, 
     createUserDocumentFromAuth, 
     signInWithGooglePopup, 
     signInAuthUserWithEmailAndPassword,
-    createAuthUserWithEmailAndPassword 
+    createAuthUserWithEmailAndPassword,
+    signOutUser
 } from '../../utils/firebase/firebase.utils'
 
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
@@ -55,6 +63,15 @@ export function* signUp({ payload: { email, password, displayName } }) {
     }
 }
 
+export function* signOut() {
+    try {
+        yield call(signOutUser)
+        yield put(signOutSuccess())
+    } catch (error) {
+        yield put(signOutFailed(error))
+    }
+}
+
 export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
     yield call(getSnapshotFromUserAuth, user, additionalDetails)
 }
@@ -79,12 +96,17 @@ export function* onSignUpSuccess() {
     yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp)
 }
 
+export function* onSignOutStart() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut)
+}
+
 export function* userSagas() {
     yield all([
         call(onCheckUserSession), 
         call(onGoogleSignInStart), 
         call(onEmailSignInStart), 
         call(onSignUpStart), 
-        call(onSignUpSuccess)
+        call(onSignUpSuccess),
+        call(onSignOutStart)
     ])
 }
