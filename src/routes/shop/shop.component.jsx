c59e4ae8cbaf7fe2827d5158a629c4205b9f5cbf
Peter Weiser <peterweiser87@gmail.com>
2023-02-21 20:07:06 +0200

    Category Saga made.

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { fetchCategoriesAsync } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from '../../store/categories/category.action';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
@@ -10,7 +10,7 @@ const Shop = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-      dispatch(fetchCategoriesAsync())
+      dispatch(fetchCategoriesStart())
   }, []);
 
   return (
@@ -21,4 +21,4 @@ const Shop = () => {
   );
 };
 
-export default Shop;
+export default Shop; 
