f096a173eaf232f1fcfb15b87a962661fac87710
Peter Weiser <peterweiser87@gmail.com>
2023-02-27 23:37:59 +0200

    Rewrote category reducer in TypeScript.

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,6 @@
-import { CATEGORIES_ACTION_TYPES, Category } from './category.types'
-import { CategoryAction } from './category.action'
+import { AnyAction } from 'redux'
+import { Category } from './category.types'
+import { fetchCategoriesStart, fetchCategoriesSuccess, fetchCategoriesFailed } from './category.action'
 
 export type CategoriesState = {
     readonly categories: Category[];
@@ -13,15 +14,21 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
     error: null
 }
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as CategoryAction) => {
-    switch(action.type) {
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-            return { ...state, isLoading: true }
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-            return { ...state, categories: action.payload, isLoading: false }
-        case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-            return { ...state, error: action.payload, isLoading: false}
-        default:
-            return state
+export const categoriesReducer = (
+    state = CATEGORIES_INITIAL_STATE, 
+    action = {} as AnyAction
+): CategoriesState => {
+    if(fetchCategoriesStart.match(action)) {
+        return { ...state, isLoading: true }
     }
+
+    if(fetchCategoriesSuccess.match(action)) {
+        return { ...state, categories: action.payload, isLoading: false }
+    }
+
+    if(fetchCategoriesFailed.match(action)) {
+        return { ...state, error: action.payload, isLoading: false}
+    }
+
+    return state
 }
