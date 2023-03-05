bdffa3fbfd6c67550fdd8e14685ab67e9296f2c1
Peter Weiser <peterweiser87@gmail.com>
2023-03-05 21:04:35 +0200

    Added PWA option.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
